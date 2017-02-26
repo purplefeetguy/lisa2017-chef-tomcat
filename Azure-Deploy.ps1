332315525169013d5080b4e9e0d91b857409b026
josh-hetland <josh.hetland@gmail.com>
2017-02-26 10:29:10 -0600

    Added premium flag which alters the disk type. Added vm group parameter object values

--- Azure-Deploy.ps1
+++ Azure-Deploy.ps1
@@ -44,8 +44,8 @@ param(
     [Parameter(Mandatory=$false)]
     [string] $SubscriptionName,
     [Parameter(Mandatory=$false)]
-    [ValidateSet("West US","East US","North-Central US")]
-    [string] $Location = "West US",
+    [ValidateSet("West US","East US","North Central US")]
+    [string] $Location = 'West US',
     [Parameter(Mandatory=$false)]
     [string] $SubnetName,
     [Parameter(Mandatory=$false)]
@@ -56,16 +56,11 @@ param(
     [int] $VmIndex = 1,
     [Parameter(Mandatory=$false)]
     [ValidateSet("xsmall","small","medium","large","xlarge")]
-    [string] $VmSize = "small",
+    [string] $VmSize = 'small',
     [Parameter(Mandatory=$false)]
     [int] $VmCount = 1,
-
-    # alternately i could move this to a different script
-    # to keep the logic simple
     [Parameter(Mandatory=$false)]
-    [string] $VmName,
-    [Parameter(Mandatory=$false)]
-    [bool] $Wipe
+    [bool] $Premium = $false
 )
 
 Import-Module '.\Functions-AzureGeneral.psm1'
@@ -124,9 +119,9 @@ for( $i = $VmIndex; $i -le ( $VmIndex + $VmCount ); $i++ )
 
 if( $ClashingNames.Length -gt 0 )
 {
-  Write-Host "ERROR: The following existing VMs were found with overlapping names:"
+  Write-Host 'ERROR: The following existing VMs were found with overlapping names:'
   Write-Host "       $($ClashingNames -Join ', ')"
-  Write-Host "       Adjust your range values and try again."
+  Write-Host '       Adjust your range values and try again.'
   exit
 }
 
@@ -156,7 +151,7 @@ if ($ResourceGroup -eq $null)
 # the resource group that is named after the resource group with diagstore on the end
 # (with dashes removed since storage accounts cant have those)
 #
-$DiagStorageName = ($ResourceGroupName -Replace '-') + "diagstore01"
+$DiagStorageName = ($ResourceGroupName -Replace '-') + 'diagstore01'
 $DiagStorage     = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $DiagStorageName -ErrorAction Ignore
 if( $DiagStorage -eq $null )
 {
@@ -165,7 +160,7 @@ if( $DiagStorage -eq $null )
   $DiagStorageParameters = @{
     location=$Location;
     storAcctName=$DiagStorageName;
-    storAcctType="Standard_LRS";
+    storAcctType='Standard_LRS';
   }
 
   New-AzureRMResourceGroupDeployment -Name "$($ResourceGroupName)-diag-$(Get-Date)-" `
@@ -180,9 +175,49 @@ $AzureSize = Get-MappedTshirtSize -TshirtSize $VmSize
 $OsSize    = 128
 $DataSize  = Get-MappedDataSize -TshirtSize $VmSize
 
+$DiskType  = 'Standard_LRS'
+if( $Premium )
+{
+  $DiskType = 'Premium_LRS'
+}
 
 
-# @NOTE: one issue is going to be with translating the latest to a timestamp value so it can be kept the same
+#
+# Create the group of virtual machines
+#
+$VmParameters = @{
+  location=$Location;
+
+  # These need to be adjusted or removed
+  tagAppNameValue='DynamicDeployment';
+  tagAppEnvValue='DynamicDeployment';
+  tagSecZoneValue='DynamicDeployment';
+
+  vmNamePrefix=$VmPrefix;
+  vmIndexOffset=$VmIndex;
+  vmCount=$VmCount;
+  vmSize=$AzureSize;
+  osDiskSizeInGB=$OsSize;
+  dataDiskSizeInGB=$DataSize;
+
+  # This will need to be specified
+  managedDiskType=$DiskType;
+
+  # This should be able to be specified as well
+  # maybe as part of a standard selection where you say linux or windows
+  imagePublisher='RedHat';
+  imageOffer='RHEL';
+  imageVersion='7.3';
+  imageRelease='latest'; # @TODO: how do i find out which one was used for storage?
+
+  adminUserName='wagsadmin';
+  adminPassword='Welcome123!';
+  vnetResGrp=$VnetRgName;
+  vnetName=$VnetName;
+  subnetName=$SubnetName;
+  diagStorAcctName=$DiagStorageName;
+}
+
 
 
 
